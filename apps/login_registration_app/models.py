6ab5d39d7ba11707eccb2eb6009b8ab05159c5b8
Lorman Lau <lormanlau@gmail.com>
2017-07-21 14:56:44 -0700

    created User Manager

--- apps/login_registration_app/models.py
+++ apps/login_registration_app/models.py
@@ -1,7 +1,24 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.db import models
+import re
+
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+
+class UserManager(models.Manager):
+	def validate(self, user_data):
+		errors = {}
+		
+		for something in user_data:
+			if len(something) < 3:
+				errors['length'] = "Items can not be less than 3 characters"
+		if not EMAIL_REGEX.match(user_data['email']):
+			errors['email'] = "Invalid email"
+		if user_data['pass'] != user_data['con_pass']:
+			errors['password'] = "Password does not match"
+
+		return errors
+		
 
 # Create your models here.
 class Users(models.Model):
