3a102724fefb7fffdd4f5a48202abcaa882e6d46
Lorman Lau <lormanlau@gmail.com>
2017-07-21 15:47:41 -0700

    added login logout

--- apps/login_registration_app/views.py
+++ apps/login_registration_app/views.py
@@ -2,18 +2,65 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render, redirect
+from django.contrib import messages
+from .models import * 
+import bcrypt
 
 # Create your views here.
 def index(request):
+	if request.session['user_id'] != 0:
+		return redirect('/quotes')
 	return render(request, 'login_registration_app/index.html')
 
 def register(request):
 	if request.method == "POST":
-		Users.objects.validate(request.POST)
+		errors = Users.objects.validate(request.POST)
+		if errors:
+			for tags, error in errors:
+				messages.error(request, error, extra_tags = tags)
+		else:
+			if len(Users.objects.filter(email = request.POST['email'])) > 0:
+				messages.error(request, "Email is already taken" , extra_tags = "email")
+			else:
+				hashed_pw = bcrypt.hashpw(request.POST['pass'].encode(), bcrypt.gensalt())
+				user = Users.objects.create(name = request.POST['name'], alias = request.POST['alias'], email= request.POST['email'], password = hashed_pw, bday = request.POST['bday'])
+				request.session['user_id'] = user.id
+				request.session['user_alias'] = user.alias
+				return redirect('/quotes')
+	return redirect('/')
+
+def logout(request):
+	request.session['user_id'] = 0
+	return redirect('/')
+
+def login(request):
+	user = Users.objects.filter(email = request.POST['email'])
+	if user:
+		if bcrypt.checkpw(request.POST['pass'].encode(), user.first().password.encode()):
+			request.session['user_id'] = user.first().id
+			request.session['user_alias'] = user.first().alias
+			return redirect('/quotes')
+
+	messages.error(request, "Invalid Email or Password" , extra_tags = "email")
 	return redirect('/')
 
 def dashboard(request):
+	if request.session['user_id'] == 0:
+		messages.error(request, "You are not Logged In", extra_tags = 'Not Logged in')
+		return redirect('/')
 	return render(request, 'login_registration_app/dashboard.html')
+
+def addquote(request):
+	if request.method == "POST":
+		if len(request.POST['name']) < 3:
+			messages.error(request, "Author needs to be at least 3 letters long", extra_tags = 'author')
+		if len(request.POST['text']) < 10:
+			messages.error(request, "Message needs to be at least 10 letters long", extra_tags = 'text')
+
+		if not messages:
+			Quote.objects.create(author = request.POST['name'], text = request.POST['text'], user = Users.objects.get(id = request.session['user_id']))
+
+	return redirect('/quotes')
 
 def userinfo(request, id):
 	return render(request, 'login_registration_app/user_info.html')
